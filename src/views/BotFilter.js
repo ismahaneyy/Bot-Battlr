9c82108e2c8badeb42da3f08cbd14d93c585e8ba
ismahaneyy <liciousmayrah@gmail.com>
2023-01-23 04:29:34 +0300

    created a form to filter bots according to their classes

---
+++ src/views/BotFilter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function BotFilter(props) {
+return (
+<form>
+<input type="checkbox"></input>
+<input type="checkbox"></input>
+<input type="checkbox"></input>
+</form>
+);
+}
+export default BotFilter;
